cb1c02e90b95a77cd96e1ab0fafc7fcc2c8217bb
soulmou-dev <oulmou.salim@gmail.com>
2026-02-10 16:06:09 +0100

    correction de ValidPasswordException

--- app/Identity/Domain/Exception/ValidationPasswordException.php
+++ app/Identity/Domain/Exception/ValidationPasswordException.php
@@ -6,7 +6,7 @@ use App\Shared\Domain\Exception\AbstractDomainException;
 
 final class ValidationPasswordException extends AbstractDomainException
 {
-    private const DEFAULT_MESSAGE = 'Les mots de pas ne correspondanet pas';
+    private const DEFAULT_MESSAGE = 'Les mots de passe ne correspondanet pas';
     private const DEFAULT_CODE_ERROR = 'PASSWORD_MISMATCH';
     private const DEFAULT_REASON = 'VALIDATION_ERROR';
 
