519893c952cf4dbaf200e91f2a70f0498fbef542
soulmou-dev <oulmou.salim@gmail.com>
2026-02-10 12:42:12 +0100

    update route web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 })->name('home');
 
 //routes pour AuhController
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->name('login.form');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
