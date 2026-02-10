07004ed8ffd1b849630aae68bc82871cc11efa31
soulmou-dev <oulmou.salim@gmail.com>
2026-02-10 16:02:59 +0100

    correction command bus et handler domain exception

--- app/Shared/Infrastructure/Bus/LaravelCommandBus.php
+++ app/Shared/Infrastructure/Bus/LaravelCommandBus.php
@@ -4,11 +4,29 @@ namespace App\Shared\Infrastructure\Bus;
 
 use App\Shared\Domain\Bus\CommandBus;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Container\Container;
 
 final class LaravelCommandBus implements CommandBus
 {
+    public function __construct(private Container $container) {}
+
     public function dispatch(object $command): void
     {
-        Bus::dispatchSync($command);
+        // Déterminer le Handler correspondant
+        $handlerClass = str_replace('Command', 'Handler', get_class($command));
+
+        // Récupérer le Handler via le container (injection automatique)
+        $handler = $this->container->make($handlerClass);
+
+        // Créer un Job Laravel inline pour utiliser dispatchSync
+        $job = new class($handler, $command) {
+            public function __construct(private $handler, private $command) {}
+            public function handle() {
+                ($this->handler)($this->command);
+            }
+        };
+
+        //Exécuter le job via Laravel Bus::dispatchSync
+        Bus::dispatchSync($job);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -14,5 +15,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+       
     })->create();
